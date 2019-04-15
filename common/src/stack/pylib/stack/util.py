2bff78cb1fbeaaee2f0b420b42033fc33a963f91
Bill Sanders <billysanders@gmail.com>
2019-04-15 13:04:23 -0700

    INTERNAL: branch manager fail.  I forgot to git-pull after requested changes were made.

--- common/src/stack/pylib/stack/util.py
+++ common/src/stack/pylib/stack/util.py
@@ -145,5 +145,26 @@ def system(cmd):
 def blank_str_to_None(string):
 	if isinstance(string, str) and string.strip() == '':
 		return None
-	
+
 	return string
+
+def unique_everseen(iterable, key=None):
+	"""List unique elements, preserving order. Remember all elements ever seen.
+
+	unique_everseen('AAAABBBCCDAABBB') --> A B C D
+	unique_everseen('ABBCcAD', str.lower) --> A B C D
+
+	Source: https://docs.python.org/3/library/itertools.html#itertools-recipes
+	"""
+	seen = set()
+	seen_add = seen.add
+	if key is None:
+		for element in itertools.filterfalse(seen.__contains__, iterable):
+			seen_add(element)
+			yield element
+	else:
+		for element in iterable:
+			k = key(element)
+			if k not in seen:
+				seen_add(k)
+				yield element
