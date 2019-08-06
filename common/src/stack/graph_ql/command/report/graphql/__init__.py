1ba8b3f2b9b010c8dd6d72664d6a86fb56514494
Mike Bobadilla <mike.bobadilla@teradata.com>
2019-08-06 15:23:01 -0700

    Graphql schema based on database schema

--- common/src/stack/graph_ql/command/report/graphql/__init__.py
+++ common/src/stack/graph_ql/command/report/graphql/__init__.py
@@ -7,6 +7,8 @@
 
 import re
 import stack.commands
+from stack.db import db
+from pprint import pprint
 
 class command(stack.commands.HostArgumentProcessor,
 	stack.commands.report.command,
@@ -15,27 +17,113 @@ class command(stack.commands.HostArgumentProcessor,
 
 class Command(command):
 	"""
-	Output the switch configuration file.
-
-	<arg type='string' name='switch'>
-	Name of the switch. If no switches are supplied, then output the report for all switches.
-	</arg>
-
-	<param type='boolean' name='nukeswitch' optional='1'>
-	If 'yes', then put the switch into a default state (e.g., no vlans, no partitions),
-	Just a "flat" switch.
-	Default: no
-	</param>
-
-	<example cmd='report switch ethernet-1-1'>
-	Output the configation file for ethernet-1-1.
-	</example>
+	Output the GraphQL schema definition (SDL) generated with the database schema.
 	"""
 
+
+	def get_table_names(self):
+		"""Returns a list of the table names in the database"""
+		db.execute("SHOW tables")
+
+		table_names = []
+		for table in db.fetchall():
+			table_names.append(list(table.values())[0])
+
+		return table_names
+
+	def get_table_description(self, table_name):
+		"""
+		return list of descriptions
+		{
+			'Default': None,
+			'Extra': 'auto_increment',
+			'Field': 'id',
+			'Key': 'PRI',
+			'Null': 'NO',
+			'Type': 'int(11)'
+		}
+		"""
+		db.execute(f'DESCRIBE {table_name}')
+
+		return db.fetchall()
+
+
+	def get_table_references(self, table_name):
+		"""
+		return list of references
+		{
+			'column_name': 'node_id',
+			'referenced_column_name': 'ID',
+			'referenced_table_name': 'nodes',
+			'table_name': 'scope_map'
+		}
+		"""
+		db.execute(f'SELECT table_name, referenced_table_name, column_name, referenced_column_name from information_schema.KEY_COLUMN_USAGE where table_name="{table_name}"')
+
+		return db.fetchall()
+
+	def get_scalar_value(self, string, required = False):
+		scalars = {
+			'varchar': 'String',
+			'text': 'String',
+			'int': "Int",
+			'enum': 'String', # TODO: Create enum here
+		}
+
+		check_string = string.lower()
+
+		for scalar in scalars:
+			if check_string.startswith(scalar):
+				ext = '!' if required else ''
+				return f"{scalars[scalar]}{ext}"
+
+	def generate_type_fields(self, table_name, description, reference):
+		fields = []
+		for field in description:
+			field_name = self.camel_case_it(field['Field'])
+			field_type = self.get_scalar_value(field['Type'], field['Null'] == 'NO')
+			fields.append((field_name, field_type))
+
+		return fields
+
+	def generate_type_field_strings(self, field_types):
+		"""Returns field types "Id: String" """
+		fields = map(lambda field: "  {}: {}".format(*field), field_types)
+		return "\n".join(fields)
+
+	def camel_case_it(self, string, delimeter = "_"):
+		"""Return string in camelCase form"""
+		string = "".join([word.capitalize() for word in string.split(delimeter)])
+		return string[0].lower() + string[1:]
+
+	def pascal_case_it(self, string, delimeter = "_"):
+		"""Return string in PascalCase form"""
+		return "".join([word.capitalize() for word in string.split(delimeter)])
+
+	def generate_sdl(self):
+
+		gql_types = []
+		for table_name in self.get_table_names():
+			description = self.get_table_description(table_name)
+			reference = self.get_table_description(table_name)
+			gql_types.append({
+				self.pascal_case_it(table_name): self.generate_type_fields(table_name, description, reference)
+			})
+
+		types_list = []
+		for gql_type in gql_types:
+			for type_name, type_values in gql_type.items():
+				types_list.append("type %s {\n%s\n}\n" % (type_name, self.generate_type_field_strings(type_values)))
+
+		return types_list
+
+# x = "\n".join(generate_sdl())
+# print(x)
+
 	def run(self, params, args):
-		print("Asdfasdfasdfasdf")
 
 		self.beginOutput()
 
+		self.addOutput(self, "\n".join(self.generate_sdl()))
 
 		self.endOutput(padChar='', trimOwner=True)
