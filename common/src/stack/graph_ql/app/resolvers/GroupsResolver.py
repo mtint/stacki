f4a971dd54f8dbf7add2fe25a4b2913da089eb21
bgreenb <bri.greenb@gmail.com>
2019-09-11 10:40:43 -0700

    FEATURE: Add groups resolvers and schema

---
+++ common/src/stack/graph_ql/app/resolvers/GroupsResolver.py
@@ -0,0 +1,58 @@
+# @copyright@
+# Copyright (c) 2006 - 2019 Teradata
+# All rights reserved. Stacki(r) v5.x stacki.com
+# https://github.com/Teradata/stacki/blob/master/LICENSE.txt
+# @copyright@
+
+from ariadne import ObjectType, QueryType, MutationType
+import app.db as db
+
+query = QueryType()
+mutation = MutationType()
+#group = ObjectType("Group")
+host = ObjectType("Host")
+
+@query.field('groups')
+def resolve_group(*_):
+	results, _ = db.run_sql("SELECT id, name FROM groups")
+	return results
+
+@host.field('name')
+def resolve_host_group(obj):
+	cmd ="""
+		SELECT * FROM nodes WHERE id=(
+		SELECT nodeid FROM memberships WHERE
+		groupid=%s)
+		"""
+	args = [obj.get('id')]
+	results, _ = db.run_sql(cmd, args)
+	return results
+
+@mutation.field('addGroup')
+def resolve_add_group(_, info, name):
+	if name in [group['name'] for group in resolve_group()]:
+		raise Exception(f'Group {name} is already defined')
+
+	cmd = 'INSERT INTO groups (name) VALUES(%s)'
+	args = (name)
+	results, _ = db.run_sql(cmd, args)
+
+	# Get the recently inserted value
+	cmd = "SELECT id, name FROM groups WHERE name=%s"
+	args = (name, )
+	results, _ = db.run_sql(cmd, args, fetchone=True)
+	return results
+
+@mutation.field('deleteGroup')
+def resolve_delete_group(_, info, name):
+	cmd = "DELETE FROM groups WHERE name=%s"
+	args = (name, )
+	_, affected_rows = db.run_sql(cmd, args)
+
+	# Return false if the db can't find the row to delete
+	if not affected_rows:
+		return False
+
+	return True
+
+object_types = [host]
